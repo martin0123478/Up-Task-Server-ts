e8c9dc5970be0f61a6fa2b828ff21159d801ccbc
martin0123478 <martin_agui97@outlook.es>
2024-11-29 22:02:40 -0600

    feat:add diferents status in tasks

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,6 +1,14 @@
 import mongoose, { Schema, Document, Types } from 'mongoose'
 
+const taskStatus = {
+    PENDING: 'pending',
+    ON_HOLD: 'onHold',
+    IN_PROGRESS: 'inProgress',
+    UNDER_REVIEW: 'underReview',
+    COMPLETED: 'completed'
+} as const
 
+export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
 export interface ITask extends Document {
     name: string
     description: string,
@@ -21,6 +29,11 @@ export const TaskSchema: Schema = new Schema({
     project: {
         type: Types.ObjectId,
         ref: 'Project'
+    },
+    status: {
+        type: String,
+        enum: Object.values(taskStatus),
+        default: taskStatus.PENDING
     }
 
 }, { timestamps: true })
