47f14a07d19fbc6dbf4109e2c7b50643e7dd171f
martin0123478 <martin_agui97@outlook.es>
2025-01-06 17:17:50 -0600

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import projectRoutes from './routes/ProjectRoutes'
 import { conncectDB } from './config/db'
+import { corsConfig } from './config/cors'
 
 dotenv.config()
+
 conncectDB(process.env.DATABASE_URL)
 const app = express()
+app.use(cors(corsConfig))
 app.use(express.json())
 app.use('/api/projects', projectRoutes)
 export default app
